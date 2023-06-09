b21efc8d1610519a200721cfc710a10ebdce7e8a
Natasha <just.too.it@gmail.com>
2023-06-09 18:03:14 +0300

    декомпозиция конфига, опции конфигурации

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import webpack  from "webpack";
+
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolves } from "./buildResolves";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig (options: BuildOptions): webpack.Configuration {
+    const {mode, paths} = options;
+
+return {
+    mode: mode,
+    entry: paths.entry,
+    output: {
+        filename: '[name].[contenthash].js',
+        path: paths.build,
+        clean: true
+    },
+    plugins: buildPlugins(options),
+      module: {
+        rules: buildLoaders(),
+      },
+      resolve: buildResolves(),
+  }
+}
