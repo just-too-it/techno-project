e96839eaa1f0cd46a9951576d68ae94c4d6599b6
Natasha <just.too.it@gmail.com>
2023-06-30 21:38:01 +0300

    организация стилей, добавление тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext, useState } from 'react'
 import { Counter } from './components/Counter';
-import './index.scss';
+import './styles/index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import AboutPage from './pages/AboutPage/AboutPage';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
+
 
 export const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О компании</Link>
       <Suspense fallback={<div>Loading...</div>}>
