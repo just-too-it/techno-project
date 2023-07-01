2b6aef48feaf5f2bd5e287fc263399d4c306ab6a
Natasha <just.too.it@gmail.com>
2023-07-01 18:43:57 +0300

    добавлена функция аналог classnames

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React, { Suspense, useContext, useState } from 'react'
-import { Counter } from './components/Counter';
 import './styles/index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
-import AboutPage from './pages/AboutPage/AboutPage';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { Theme, ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
-
-
+import { classNames } from './helpers/classNames/classNames';
 
 export const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О компании</Link>
