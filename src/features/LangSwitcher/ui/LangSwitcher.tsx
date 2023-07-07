4a2c0a2ed9f03ffa09ff27f282b7abe81d8f1754
Natasha <just.too.it@gmail.com>
2023-07-07 13:42:01 +0300

    add stylelint, i18n plugin for eslint, fix all styles

--- src/features/LangSwitcher/ui/LangSwitcher.tsx
+++ src/features/LangSwitcher/ui/LangSwitcher.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonVariant } from 'shared/ui/Button/Button';
-import styles from  './LangSwitcher.module.scss'
 
 interface LangSwitcherProps {
   className?: string;
@@ -18,7 +17,7 @@ export const LangSwitcher: FC<LangSwitcherProps> = ({ className }) => {
   return (
     <Button 
       onClick={SwitchLanguages} 
-      className={classNames(styles.LangSwitcher, {}, [className])}
+      className={classNames('', {}, [className])}
       variant={ButtonVariant.CLEAR}
     >{t('Язык')}</Button>
   )
