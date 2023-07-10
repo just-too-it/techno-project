d59062722476d7b6f0513b9906e338544f29a07b
Natasha <just.too.it@gmail.com>
2023-07-10 18:23:26 +0300

    add not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTranslation } from "react-i18next";
+import styles from "./NotFoundPage.module.scss";
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage: FC<NotFoundPageProps> = ({ className }) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(styles.NotFoundPage, {}, [className])}>
+      {t("Страница не найдена")}
+    </div>
+  );
+};
