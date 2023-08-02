24b1e22bfbe2116eb581629c772d9d3486eb9fbe
Natasha <just.too.it@gmail.com>
2023-08-02 20:58:10 +0300

    fix screenshot, styles

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Modal } from "shared/ui/Modal/Modal";
-import styles from "./LoginModal.module.scss";
 import { LoginForm } from "../LoginForm/LoginForm";
 
 interface LoginModalProps {
@@ -15,7 +14,7 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
 
   return (
     <Modal
-      className={classNames(styles.LoginModal, {}, [className])}
+      className={classNames("", {}, [className])}
       isOpen={isOpen}
       onClose={onClose}
       lazy
